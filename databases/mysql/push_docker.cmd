5544e7495acd5959c8331c24f709e74bbfa24786
Ivan Gudak <ivan.gudak@dynatrace.com>
2024-01-15 13:09:38 +0100

    add dependency calls from tasks to categories

---
+++ databases/mysql/push_docker.cmd
@@ -0,0 +1,2 @@
+docker image build --platform linux/amd64 -t ivangudak096/wp-mysql:latest .
+docker push ivangudak096/wp-mysql:latest
---
+++ databases/postgres/push_docker.cmd
@@ -0,0 +1,2 @@
+docker image build --platform linux/amd64 -t ivangudak096/wp-postgres:latest .
+docker push ivangudak096/wp-postgres:latest
