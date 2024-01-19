308ff2a73498210cea226d8c4b29e6b701598dc1
Ivan Gudak <ivan.gudak@dynatrace.com>
2024-01-19 13:59:30 +0100

    switch to apache http client

---
+++ users/src/main/java/eu/dec21/wp/users/config/RestTemplateConfig.java
@@ -0,0 +1,26 @@
+package eu.dec21.wp.users.config;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        clientHttpRequestFactory.setHttpClient(HttpClients.createDefault());
+        return clientHttpRequestFactory;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+                .requestFactory(this::clientHttpRequestFactory)
+                .build();
+    }
+
+}
