724456356e296e57018988d55f4b41930b396c8a
Ivan Gudak <ivan.gudak@dynatrace.com>
2024-04-08 13:25:53 +0200

    Sync with the private version in bitbucket

--- tasks/src/test/java/eu/dec21/wp/tasks/TasksApplicationTest.java
+++ tasks/src/test/java/eu/dec21/wp/tasks/TasksApplicationTest.java
@@ -1,11 +1,17 @@
 package eu.dec21.wp.tasks;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+
+//@AutoConfigureDataMongo
 @SpringBootTest
 @ActiveProfiles("test")
+@ExtendWith(SpringExtension.class)
+//@ActiveProfilesutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE, connection = EmbeddedDatabaseConnection.NONE)
 public class TasksApplicationTest {
 
     @Test
