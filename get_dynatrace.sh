07c6f859c6079029fecc214a4e91c5a9f4b097d7
Ivan Gudak <ivan.gudak@dynatrace.com>
2024-07-27 16:43:42 +0200

    Simplify K8s. Add resource attributes for enrichment

--- get_dynatrace.sh
+++ get_dynatrace.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-PLATFORM=$(uname -p);
-export PLATFORM;
-if [ "$PLATFORM" = "arm" ] || [ "$PLATFORM" = "arm64" ] || [ "$PLATFORM" = "aarch64" ]; then
-  export PLATFORM="arm";
-elif [ $PLATFORM = "x86" ] || [ $PLATFORM = "x86_64" ] || [ $PLATFORM = "x64" ]; then
-  export PLATFORM="x86";
-else
-  export PLATFORM="x86";
-fi
+DT_TENANT_URL=https://<tenant>.dev.dynatracelabs.com
+DT_TOKEN=<token>
 
-wget -O linux-agent.zip http://graalnative.eastus.cloudapp.azure.com:8080/linux-$PLATFORM.zip
-wget -O buildtools.zip http://graalnative.eastus.cloudapp.azure.com:8080/buildtools.zip
-mkdir plugin && mv buildtools.zip ./plugin/ && cd ./plugin && unzip ./buildtools.zip
+curl -X GET "$DT_TENANT_URL/api/v1/deployment/installer/agent/unix/paas/latest?flavor=default&arch=x86&bitness=64&include=java-graal-native&skipMetadata=true" -H "accept: application/octet-stream"  -H "Authorization: Api-Token $DT_TOKEN" -o linux-agent.zip
+
---
+++ web/week-planner-web/start_web.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ng serve --host 0.0.0.0 --disable-host-check -c development
+
