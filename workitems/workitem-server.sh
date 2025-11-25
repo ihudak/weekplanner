27f4745e29856a8d2a48a68f0a254a5b0b01f8a7
Ivan Gudak <ivan.gudak@dynatrace.com>
2025-11-25 08:07:34 +0100

    Load generator scripts

--- workitems/workitem-server.sh
+++ workitems/workitem-server.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cd workitems/build/libs
+export WP_SERVER_PORT=8084
+export WP_PROFILE=dev
 export WP_WRKITM_CNT=300
 export OTEL_SERVICE_NAME=WorkItems
 java -Dotel.service.name=$SERVICE_FULL_NAME -jar ./workitems-0.0.1-SNAPSHOT.jar
